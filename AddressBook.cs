1b1028913a66bb6fb20e904fda25817038687836
BhagyashriSabale <balisabale16@gmail.com>
2023-05-25 05:21:25 +0530

    [Bhagyashri] Add new contact in address book

---
+++ AddressBook.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class AddressBook
+    {
+        private List<Contact> contacts;
+
+        public AddressBook()
+        {
+            contacts = new List<Contact>();
+        }
+        public void AddContact()
+        {
+            Console.WriteLine("Enter contact details:");
+            Console.Write("First Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Address: ");
+            string address = Console.ReadLine();
+            Console.Write("City: ");
+            string city = Console.ReadLine();
+            Console.Write("State: ");
+            string state = Console.ReadLine();
+            Console.Write("ZIP: ");
+            string zip = Console.ReadLine();
+            Console.Write("Phone Number: ");
+            string phoneNumber = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            contacts.Add(contact);
+
+            Console.WriteLine("Contact added to the address book.");
+            Console.WriteLine();
+        }
+        public void ViewAddressBook()
+        {
+            if (contacts.Count == 0)
+            {
+                Console.WriteLine("Address book is empty.");
+            }
+            else
+            {
+                Console.WriteLine("Address Book:");
+                foreach (var contact in contacts)
+                {
+                    Console.WriteLine($"Name: {contact.FirstName} {contact.LastName}");
+                    Console.WriteLine($"Address: {contact.Address} {contact.City} {contact.State}");
+                    Console.WriteLine($"ZIP: {contact.Zip}");
+                    Console.WriteLine($"Phone Number: {contact.PhoneNumber}");
+                    Console.WriteLine($"Email: {contact.Email}");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,10 +4,11 @@ namespace AddressBook
 {
     internal class Program
     {
-        static List<Contact> addressBook = new List<Contact>();
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome Address Book");
+            AddressBook addressBook = new AddressBook();
             while (true)
             {
                 Console.WriteLine("Address Book Menu:");
@@ -19,10 +20,10 @@ namespace AddressBook
                 switch (choice)
                 {
                     case 1:
-                        AddContact();
+                        addressBook.AddContact();
                         break;
                     case 2:
-                        ViewAddressBook();
+                        addressBook.ViewAddressBook();
                         break;
                     case 3:
                         Environment.Exit(0);
@@ -31,51 +32,7 @@ namespace AddressBook
                         Console.WriteLine("Invalid choice. Please try again.");
                         break;
                 }
-            }
-        }
-        static void AddContact()
-        {
-            Console.WriteLine("Enter contact details:");
-            Console.Write("First Name: ");
-            string firstName = Console.ReadLine();
-            Console.Write("Last Name: ");
-            string lastName = Console.ReadLine();
-            Console.Write("Address: ");
-            string address = Console.ReadLine();
-            Console.Write("City: ");
-            string city = Console.ReadLine();
-            Console.Write("State: ");
-            string state = Console.ReadLine();
-            Console.Write("ZIP: ");
-            string zip = Console.ReadLine();
-            Console.Write("Phone Number: ");
-            string phoneNumber = Console.ReadLine();
-            Console.Write("Email: ");
-            string email = Console.ReadLine();
-            Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
-            addressBook.Add(contact);
 
-            Console.WriteLine("Contact added to the address book.");
-            Console.WriteLine();
-        }
-        static void ViewAddressBook()
-        {
-            if (addressBook.Count == 0)
-            {
-                Console.WriteLine("Address book is empty.");
-            }
-            else
-            {
-                Console.WriteLine("Address Book:");
-                foreach (var contact in addressBook)
-                {
-                    Console.WriteLine($"Name: {contact.FirstName} {contact.LastName}");
-                    Console.WriteLine($"Address: {contact.Address} {contact.City} {contact.State}");
-                    Console.WriteLine($"ZIP: {contact.Zip}");
-                    Console.WriteLine($"Phone Number: {contact.PhoneNumber}");
-                    Console.WriteLine($"Email: {contact.Email}");
-                    Console.WriteLine();
-                }
             }
         }
 
